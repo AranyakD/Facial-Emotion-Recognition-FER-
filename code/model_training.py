a0fd06bd3047c245635331988acc4e24075cf930
Aranyak Dutta <aranyak.dbpc@gmail.com>
2026-03-18 13:44:31 +0530

    Inital commit

---
+++ code/model_training.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+train_dir = "dataset_Train"
+test_dir = "dataset_Test"
+img_height = 48
+img_width = 48
+batch_size = 32
+
+# loading train dataset
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    train_dir,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+    color_mode="grayscale"
+)
+
+# loading test dataset
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+    color_mode="grayscale"
+)
+
+# normalization layer
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+# applying normalization to train_dataset and test_dataset
+train_dataset = train_dataset.map(lambda x, y: (normalization_layer(x), y))
+test_dataset = test_dataset.map(lambda x, y: (normalization_layer(x), y))
+
+""" # verifying class labels
+class_names = train_dataset.class_names
+print("Emotion Classes:", class_names) """
+
+""" for images, labels in train_dataset.take(1):
+    print("min pixel val:", tf.reduce_min(images))
+    print("max pixel val:", tf.reduce_max(images)) """
+
+# Defining CNN model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
+                           input_shape=(48, 48, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+
+    tf.keras.layers.Dense(7, activation='softmax')
+])
+
+model.summary()
+
+# compling the model
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# fixing overfitting
+early_stop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=3,
+    restore_best_weights=True
+)
+
+# training the model
+history = model.fit(
+    train_dataset,
+    validation_data=test_dataset,
+    epochs=20,
+    callbacks=[early_stop]
+)
+
+# checking for improvements
+plt.plot(history.history['accuracy'], label='train')
+plt.plot(history.history['val_accuracy'], label='validation')
+plt.legend()
+plt.show()
